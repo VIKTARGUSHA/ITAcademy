9f41dc4441658b9d6d16468950f6128b8a091e71
gusha <victorgusha1@gmail.com>
2022-03-11 22:42:09 +0300

    lesson3

---
+++ src/by/academy/Lesson3.java
@@ -0,0 +1,17 @@
+package by.academy;
+
+public class Lesson3 {
+
+        public static void main(String[] args) {
+            int x =(int) Math.random()*100;
+            if (x%2 == 0){
+                System.out.println("Чётное");
+            }
+            else
+            {
+                System.out.println("Нечётное");
+            }
+        }
+    }
+
+}
