2b55cac260f72c440db3e597b42d5fbdcba50861
VAbukhovich <victorgusha1@gmail.com>
2022-03-11 15:50:34 +0300

    lesson3 11.03.2022

---
+++ src/by/academy/Task1.java
@@ -0,0 +1,41 @@
+package by.academy;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Сумма покупки:");
+        double amount = sc.nextInt();
+        System.out.println("Возраст:");
+        int age = sc.nextInt();
+
+        if(amount < 100){
+            amount = (amount*95)/100;
+            System.out.println("1");
+        }
+        else if(amount < 200){
+            amount = (amount*93)/100;
+            System.out.println("2");
+        }
+        else if(amount < 300){
+            if (age > 18) {
+                amount = (amount * (88 - 4)) / 100;
+                System.out.println("3");
+            }
+            else {
+                amount = amount * (88 + 3) / 100;
+                System.out.println("33");
+            }
+        }
+        else if (amount < 400){
+            amount = (amount*85)/100;
+            System.out.println("4");
+        }
+        else if(amount >= 400){
+            amount = (amount * 80)/100;
+            System.out.println("5");
+        }
+        System.out.println("Сумма со скидкой" + ": " + amount);
+    }
+}
