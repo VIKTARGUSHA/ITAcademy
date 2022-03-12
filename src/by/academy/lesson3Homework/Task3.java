87e33f6c54e37dba33bbdb9a085ecd40d45abe7b
VAbukhovich <victorgusha1@gmail.com>
2022-03-12 16:17:32 +0300

    lesson3 12.03.2022

---
+++ src/by/academy/lesson3Homework/Task3.java
@@ -0,0 +1,16 @@
+package by.academy.lesson3Homework;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = 1;
+        System.out.println();
+        while (y < 10){
+            System.out.print(x * y + " ");
+            y++;
+        }
+    }
+}
