26cba81bfff29fa3f9611d2656995c99c92cbb0f
gusha <victorgusha1@gmail.com>
2022-03-24 21:34:32 +0300

    24.03.2022

---
+++ src/by/academy/ClassWork/Lesson7/SeaFood.java
@@ -0,0 +1,15 @@
+package by.academy.ClassWork.Lesson7;
+
+public class SeaFood extends Product{
+    String fresh;
+    String color;
+    public void setColor(String color){
+        this.color = color;
+    }
+    public void setFresh(String freshOrNot){
+        this.fresh = freshOrNot;
+    }
+    public SeaFood(){
+
+    }
+}
