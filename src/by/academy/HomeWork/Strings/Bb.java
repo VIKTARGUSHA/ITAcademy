861276bbf0ec7e63426356a6ef606e7ba2d6616a
gusha <victorgusha1@gmail.com>
2022-03-30 22:12:29 +0300

    29.03.2022

---
+++ src/by/academy/HomeWork/Strings/Bb.java
@@ -0,0 +1,9 @@
+package by.academy.HomeWork.Strings;
+
+public class Bb {
+    public static void main(String[] args) {
+        String a = "6";
+        String b = "-6";
+        System.out.println(Integer.parseInt(a) + " " + Integer.parseInt(b));
+    }
+}
