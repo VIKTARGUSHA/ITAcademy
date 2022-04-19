2575c2177e98fe4c1243da503062338801701f4d
VAbukhovich <victorgusha1@gmail.com>
2022-04-19 13:30:15 +0300

    04.04.2022

---
+++ src/by/academy/HomeWork/GenericsAndInerClasses/Task8/Demo.java
@@ -0,0 +1,7 @@
+package by.academy.HomeWork.GenericsAndInerClasses.Task8;
+
+public class Demo {
+    public static void main(String[] args) {
+    int x = 2;
+        System.out.println((char) );
+}
