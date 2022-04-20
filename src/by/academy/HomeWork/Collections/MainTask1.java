9c57eada32f2b5e4a1d4993f89c93b1d2f7ecff1
gusha <victorgusha1@gmail.com>
2022-04-20 22:37:28 +0300

    20.04

--- src/by/academy/HomeWork/Collections/MainTask1.java
+++ src/by/academy/HomeWork/Collections/MainTask1.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 public class MainTask1 {
     public static void main(String[] args) {
         ArrayList<Task1> ar = new ArrayList<>();
-        ar.add(new Task1(5));
+        ar.add(new Task1(7));
         ar.add(new Task1(7));
         ar.add(new Task1(9));
         ar.add(new Task1(9));
         ar.add(new Task1(11));
         ar.add(new Task1(5));
-        System.out.println(Task1.returnListWithoutDublicates(ar));
+        ar.add(new Task1(7));
+        ar.add(new Task1(5));
+        ar.add(new Task1(2));
+        ar.add(new Task1(11));
+        System.out.println(Task1.returnListWithoutDublicates(ar).toString());
     }
 }
--- src/by/academy/HomeWork/Collections/Task1.java
+++ src/by/academy/HomeWork/Collections/Task1.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.TreeSet;
 
-public class Task1 {
+public class Task1 implements Comparable {
+
     int special;
-    public Task1(int i){
+
+    public Task1(int i) {
         special = i;
     }
-    public static ArrayList <Task1> returnListWithoutDublicates(ArrayList<Task1> ar){
+
+    public static TreeSet<Task1> returnListWithoutDublicates(ArrayList<Task1> ar) {
         TreeSet<Task1> tree = new TreeSet<>();
         ArrayList<Task1> ar1 = new ArrayList<>();
 
-        for (int i = 0; i < ar.size(); i++ ){
+        for (int i = 0; i < ar.size(); i++) {
             tree.add(ar.get(i));
         }
-        Task1 [] array = (Task1[]) tree.toArray();
-        for (int j = 0; j < array.length; j++){
-            ar1.add(array[j]);
-        }
-        return ar1;
+       // System.out.println(tree.toString());
+        return tree;
     }
-
-
+//        Task1 [] array =  tree.toArray();
+//        for (int j = 0; j < array.length; j++){
+//            ar1.add(array[j]);
+//        }
+//        return ar1;
+//    }
+//
+//
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -31,16 +37,26 @@ public class Task1 {
         Task1 task1 = (Task1) o;
         return special == task1.special;
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(special);
-    }
-
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(special);
+//    }
+//
     @Override
     public String toString() {
         return "Task1{" +
                 "special=" + special +
                 '}';
     }
+
+        @Override
+        public int compareTo (Object o){
+            Task1 task1 = (Task1) o;
+            if (task1.special == this.special) {
+                return 0;
+            }
+            return 1;
+        }
+
 }
