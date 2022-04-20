41f528756f3654f41568f02fd664738be9913022
VAbukhovich <victorgusha1@gmail.com>
2022-04-20 16:55:14 +0300

    21.02.2022

--- src/by/academy/HomeWork/GenericsAndInerClasses/Task9/Colculator.java
+++ src/by/academy/HomeWork/GenericsAndInerClasses/Task9/Colculator.java
@@ -1,16 +1,16 @@
 package by.academy.HomeWork.GenericsAndInerClasses.Task9;
 
 public class Colculator {
-    public static <T,V> T sum (T x, V y){
-return x + y;
+    public static <T, V> double sum (T x, V y){
+return  Double.parseDouble(x.toString()) + Double.parseDouble(y.toString());
     }
-    public static < T, V> Double multiply(T x, V y){
-return (Double) x * (Double) y;
+    public static <T, V> Double multiply(T x, V y){
+return Double.parseDouble(x.toString())  * Double.parseDouble(y.toString());
     }
     public static <T,V> Double divide(T x, V y){
-return (Double) x / (Double) y;
+return Double.parseDouble(x.toString())  / Double.parseDouble(y.toString());
     }
     public static <T,V> Double subtraction(T x, V y){
-return (Double) x - (Double) y;
+return Double.parseDouble(x.toString()) - Double.parseDouble(y.toString());
     }
 }
