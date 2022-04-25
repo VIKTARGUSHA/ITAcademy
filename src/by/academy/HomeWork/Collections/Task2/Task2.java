d86fb94939ec1ff0f264f041eb090bac32883e8a
VAbukhovich <victorgusha1@gmail.com>
2022-04-25 13:54:42 +0300

    21.02.2022

--- src/by/academy/HomeWork/Collections/Task2/Task2.java
+++ src/by/academy/HomeWork/Collections/Task2/Task2.java
@@ -1,14 +1,42 @@
 package by.academy.HomeWork.Collections.Task2;
 
-import java.awt.*;
 import java.util.*;
-import java.util.List;
 
 public class Task2 {
     public static void main(String[] args) {
-        LinkedList<> linkedList = new LinkedList();
-        ArrayList<String> arrayList = new ArrayList();
-        TimeCounter.<ArrayList<String>>timeForChoose(arrayList);
+        LinkedList linkedList = new LinkedList();
+        ArrayList arrayList = new ArrayList();
+        FillIn.fillInAr(arrayList);
+        FillIn.fillInlL(linkedList);
+        FillIn.getElement(arrayList);
+        FillIn.getElement1(linkedList);
+    }
+        static class FillIn{
+            public static void fillInAr(ArrayList<Double> ar){
+                for (int i = 0; i < 1000000; i++){
+                    ar.add(Math.random()*100);
+                }
+                System.out.println("Arraylist is full of");
+            }
+            public static void fillInlL(LinkedList<Double> lL){
+                for (int i = 0; i < 1000000; i++){
+                    lL.add(Math.random()*100);
+                }
+                System.out.println("LinkedList is full of");
+            }
+            public static void getElement(ArrayList <?> ar){
+                for (int j = 0; j < 100000; j++){
+                    ar.get((int)(Math.random()*1000000));
+                }
+                System.out.println("ArrayList speed");
+            }
+            public static void getElement1(LinkedList <?> ar){
+                for (int j = 0; j < 100000; j++){
+                    ar.get((int)(Math.random()*1000000));
+                }
+                System.out.println("LinkedList speed");
+            }
+        }
 
     }
-}
+
--- src/by/academy/HomeWork/Collections/Task2/TimeCounter.java
+++ src/by/academy/HomeWork/Collections/Task2/TimeCounter.java
@@ -5,21 +5,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 
-public class TimeCounter {
-    public static <T extends List> void timeForList(T t){
-        int counter = 0;
-        while (counter < 1000000){
-            t.add("Hello World");
-            counter++;
-        }
-        System.out.println("List is fiiled");
-    }
-    public static  <T extends ArrayList &  LinkedList> void timeForChoose(T t){
-        int counter2 = 0;
-
-        while (counter2 < 100000){
-            t.get((int)(Math.random()*100000));
-            counter2++;
-        }
-    }
-}
+//public class TimeCounter {
+//    public static <T extends List> void timeForList(T t){
+//        int counter = 0;
+//        while (counter < 1000000){
+//            t.add("Hello World");
+//            counter++;
+//        }
+//        System.out.println("List is fiiled");
+//    }
+//    public static  <T extends LinkedList> void timeForChoose(T t){
+//        int counter2 = 0;
+//
+//        while (counter2 < 100000){
+//            t.get((int)(Math.random()*100000));
+//            counter2++;
+//        }
+//    }
+//}
