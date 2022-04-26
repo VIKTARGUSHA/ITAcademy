edba3841a4d4d3dd61c61bb3e176a8cc1924043c
VAbukhovich <victorgusha1@gmail.com>
2022-04-26 13:01:21 +0300

    21.02.2022

--- src/by/academy/HomeWork/Collections/LinkedList/MainLinkedList.java
+++ src/by/academy/HomeWork/Collections/LinkedList/MainLinkedList.java
@@ -8,6 +8,20 @@ public class MainLinkedList {
         linkedList.add("D");
         linkedList.add("E");
         linkedList.print();
-        System.out.println("\n" + linkedList.get(2).toString());
+        System.out.print("\n" + linkedList.get(4).toString());
+        System.out.print("\n" + linkedList.get(0).toString());
+        System.out.print("\n" + linkedList.get(1).toString());
+        System.out.println();
+        linkedList.add(3, "F");
+        linkedList.add(4, "G");
+        linkedList.add(0, "H");
+        linkedList.print();
+        linkedList.remove(1);
+        linkedList.remove(4);
+
+        linkedList.remove(linkedList.size()-1);
+        System.out.println();
+        linkedList.print();
+
     }
 }
